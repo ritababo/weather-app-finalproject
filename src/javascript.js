8f4b84483b4ac61f58c14e07eaefee0f6d0b5032
ritababo <152433690+ritababo@users.noreply.github.com>
2023-12-15 18:37:28 +0000

    Injected humidity, wind and date parameters from API

--- src/javascript.js
+++ src/javascript.js
@@ -1,16 +1,57 @@
-function refreshWeather(response) {
+function refreshWeatherApp(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let currentInfo = document.querySelector("#current-info");
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  dateElement.innerHTML = formatDate(date);
+  currentInfo.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function refreshHumidityWindApp(response) {
+  let humidityElement = document.querySelector("#humidity-info");
+  let windElement = document.querySelector("#wind-info");
+
+  humidityElement.innerHTML = `${response.data.temperature.humidity}<span class="humidity-unit" id="humidity-unit">%</span>`;
+  windElement.innerHTML = `${response.data.wind.speed}<span class="wind-unit" id="wind-unit">km/h</span>`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturdar",
+  ];
+  let day = weekdays[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "3cafb635b28334o0e324aet783836f5a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  axios.get(apiUrl).then((response) => {
+    refreshWeatherApp(response);
+    refreshHumidityWindApp(response);
+  });
 }
 
 function handleSearchSubmit(event) {
